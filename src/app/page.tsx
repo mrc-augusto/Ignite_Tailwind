4dc7ae1fc8e37d66db46d9ee970bafcace6ab414
mrc-augusto <mrc_augusto@hotmail.com>
2024-01-24 18:29:59 -0300

    tabs created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import { SettingTabs } from '@/components/SettingsTabs'
+
 export default function Home() {
-  return <h1>Meu novo projeto</h1>
+  return(
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <SettingTabs/>
+
+      <div className='mt-6 flex flex-col '>
+        <div className='flex items-center justify-between pb-5 border-b boder-zinc-200'>
+          <div className='space-y-1'>
+            <h2 className='text-lg font-medium text-zinc-900'>Personal info</h2>
+            <span className='text-sm text-zinc-500'>Update your photo and pesonal details heire</span>
+          </div>
+          <div className='flex -items-center gap-2'>
+            <button type='button' className='rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50'>Cancel</button>
+            <button type='submit' form='settings' className='rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700'>Save</button>
+          </div>
+        </div>
+        <form id='settings' className='mt-6 flex flex-col w-full'></form>
+      </div>
+    </>
+  ) 
 }
